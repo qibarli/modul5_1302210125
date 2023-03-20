ec7cd1ccacbb986002dd4e989ea80c837c9eba6d
PC14-IFLAB <barliant.rifqi20@gmail.com>
2023-03-20 07:19:40 +0700

    Initial commit

---
+++ penjumlahan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_1302210125
+{
+    internal class penjumlahan
+    {
+        public void jumlahTigaAngka<T>(T nama)
+        {
+            Console.WriteLine("13", "02", "21");
+
+        }
+    }
+}
